9bc257532744408083107829232514d6f8a4f9f3
Miquel Miró Nicolau <miquel.miro@uib.cat>
2022-05-05 17:46:55 +0200

    Now the label can be a logit value

--- uib_xai/data/mura.py
+++ uib_xai/data/mura.py
@@ -13,7 +13,9 @@ from torchvision.io import read_image
 
 
 class MuraImageDataset(Dataset):
-    def __init__(self, path: str):
+    def __init__(self, path: str, one_hot_encoding: bool = -1):
+        if one_hot_encoding > 1:
+            raise ValueError(f"Selected option for one hot encoding not valid")
         file_names = glob.glob(path)
         labels = list(map(lambda x: x.split(os.path.sep)[-2], file_names))
         is_train_set = list(map(lambda x: x.split(os.path.sep)[-3] == "train", file_names))
@@ -27,12 +29,18 @@ class MuraImageDataset(Dataset):
         self.__labels = list(map(lambda x: self.__labels_map[x], labels))
         self.__is_train_set = is_train_set
 
+        self.__one_hot_encoding: bool = one_hot_encoding
+
     def __getitem__(self, index):
         img_path = self.__file_names[index]
         image = read_image(img_path, ImageReadMode.GRAY)
 
-        label = np.array([0, 0], dtype=np.float32)
-        label[self.__labels[index]] = 1
+        if self.__one_hot_encoding < 0:
+            label = np.array([0, 0], dtype=np.float32)
+            label[self.__labels[index]] = 1
+        else:
+            label = np.array([0], dtype=np.float32)
+            label[0] = int(self.__labels[index] == self.__one_hot_encoding)
 
         return image, torch.from_numpy(label)
 
