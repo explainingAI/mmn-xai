3569ead5c808ebcdb7e4f721f4cc72d5c9b2bbaa
Miquel Miró Nicolau <miquel.miro@uib.cat>
2022-05-09 22:05:31 +0200

    Now the test is able to work for all systems

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -3,9 +3,9 @@
 
 """
 import unittest
+import os
 
 import numpy as np
-import torch
 
 from uib_xai.data import dataset
 
@@ -13,8 +13,10 @@ from uib_xai.data import dataset
 class TestDataset(unittest.TestCase):
     def test_dataset(self):
         """ Test case: the saliency map is perfectly faithfull. """
-        dts = dataset.ImageDataset([".\\un\\1.png", ".\\dos\\2.png", ".\\tres\\3.png"],
-                                     lambda x: np.zeros((10, 10)), removed_classes=["dos"])
+        dts = dataset.ImageDataset(
+            [os.path.join(".", "un", "1.png"), os.path.join(".", "dos", "2.png"),
+             os.path.join(".", "tres", "3.png")],
+            lambda x: np.zeros((10, 10)), removed_classes=["dos"])
 
         for img in dts:
             break
